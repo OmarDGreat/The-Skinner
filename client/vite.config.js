75964ad7902914e19f9909aa7d348029ed4d256d
Omar <Omar.Daghestani@colorado.edu>
2025-01-11 18:40:30 -0700

    Prepare for full-stack deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
-    outDir: 'dist', // Ensure this matches what Vercel expects
+    outDir: 'dist', // Ensure this matches the dist folder in your vercel.json
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://the-skinner-front-back.vercel.app', // Backend local server
+        changeOrigin: true,
+      },
+    },
   },
 });
